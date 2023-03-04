d5c779eddb972df5ed041a770b98d4d14aab1f3c
faverog <faverog@uwindsor.ca>
2023-03-04 01:10:40 -0500

    Added template selection dropdown

--- LaTeXReportGenerator.py
+++ LaTeXReportGenerator.py
@@ -3,6 +3,7 @@ LaTeX Report Generator
 Gian Favero
 February 2023
 '''
+import sys
 from tkinter import *
 from tkinter import filedialog
 import ctypes
@@ -25,19 +26,17 @@ class LaTeXReportGenerator:
 
         # Report Variables
         self.report_information = {
-            "File Name": "",
-            "Report Title": "",
-            "Course": "",
-            "Professor": "",
-            "Author": "",
-            "Student ID": "",
-            "Due Date": ""
+            "Template": "",
+            "Info Keys": {},
         }
 
+        self.entries = []
+        self.selected_template = ""
+
     def get_directory(self):
-        root = Tk()
-        root.withdraw()
-        root.iconbitmap(default=self.ICON_PATH)
+        self.root = Tk()
+        self.root.withdraw()
+        self.root.iconbitmap(default=self.ICON_PATH)
 
         # Get project path and create LaTeX folder
         folder_selected = filedialog.askdirectory()
@@ -46,17 +45,49 @@ class LaTeXReportGenerator:
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         
-        if not os.path.exists(folder_selected + "/Images"):
-            os.makedirs(folder_selected + "/Images")
+        if not os.path.exists(folder_selected + "/LaTeX Images"):
+            os.makedirs(folder_selected + "/LaTeX Images")
+        
+    def get_template(self):
+        self.root = Tk()
+        self.root.title("Report Template")
+        self.root.geometry("300x100")
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-    def submit_entries(self):
+        label = Label(self.root, text="Select a report template:")
+        label.grid(row=0, column=0, padx=5, pady=5)
+
+        options = ["Favero", "Capstone Report"]
+        self.selected_template = StringVar(self.root)
+
+        self.selected_template.set(options[0])
+
+        dropdown = OptionMenu(self.root, self.selected_template, *options)
+        dropdown.grid(row=0, column=1, padx=5, pady=5)
+        
+        submit_button = Button(self.root, text="Submit", command=self.submit_template_entry)
+        submit_button.grid(row=1, column=1, padx=5, pady=5)
+
+        self.root.mainloop()
+
+    def submit_template_entry(self):
+        self.report_information["Template"] = self.selected_template.get()
+        print("Template Chosen: ", self.report_information["Template"])
+        self.root.quit()
+        self.root.destroy()
+
+    def submit_info_entries(self):
         # Get the values from the entry fields
-        for index, key in enumerate(self.report_information.keys()):
-            self.report_information[key] = self.entries[index].get()
+        for index, key in enumerate(self.report_information["Info Keys"].keys()):
+            self.report_information["Info Keys"][key] = self.entries[index].get()
+        
+        print("Report Information:", self.report_information)
         self.root.quit()
     
     def on_closing(self):
         self.root.quit()
+        print("Program terminated")
+        sys.exit()
 
     def get_report_information(self):
         # Create a new tkinter self.root
@@ -65,7 +96,11 @@ class LaTeXReportGenerator:
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         # Create entry fields for the report information
-        entry_labels = ["File Name", "Report Title", "Course", "Professor", "Author", "Student Number", "Due Date"]
+        if self.report_information["Template"] == "Favero":
+            entry_labels = ["File Name", "Report Title", "Course", "Professor", "Author", "Student Number", "Due Date"]
+        elif self.report_information["Template"] == "Capstone Report":
+            entry_labels = ["File Name", "Date"]
+        self.report_information["Info Keys"] = {key: "" for key in entry_labels}
         self.entries = [Entry(self.root) for i in range(len(entry_labels))]
 
         # Set the position of the entry fields in the self.root
@@ -74,18 +109,19 @@ class LaTeXReportGenerator:
             self.entries[i].grid(row=i, column=1, padx=5, pady=5)
 
         # Create a submit button
-        submit_button = Button(self.root, text="Submit", command=self.submit_entries)
+        submit_button = Button(self.root, text="Submit", command=self.submit_info_entries)
         submit_button.grid(row=len(self.entries)+1, column=1, padx=5, pady=5)
 
         # Start the entry self.root event loop
         self.root.mainloop()
 
-    def generate_report(self, style):
-        if style == "Favero":
+    def generate_report(self):
+        if self.report_information["Template"] == "Favero":
+            print("Generating Favero Report...")
             with open('LaTeX Templates/Favero/FaveroTemplate.tex', 'r') as template_file:
                 template = template_file.read()
-                for key in self.report_information.keys():
-                    template = template.replace(f'${key}$', self.report_information[key])
+                for key in self.report_information["Info Keys"].keys():
+                    template = template.replace(f'${key}$', self.report_information["Info Keys"][key])
                 new_report = template
 
                 filepath = os.path.join(self.path, self.report_information["File Name"] + ".tex")
@@ -95,3 +131,18 @@ class LaTeXReportGenerator:
             shutil.copyfile('LaTeX Templates/Favero/mcode.sty', os.path.join(self.path, 'mcode.sty'))
             shutil.copyfile('LaTeX Templates/Favero/matlab.sty', os.path.join(self.path, 'matlab.sty'))
 
+            print("Report Generated Successfully!")
+        
+        elif self.report_information["Template"] == "Capstone Report":
+            print("Generating Capstone Report...")
+            with open('LaTeX Templates/Capstone Progress Updates/ProgressReport.tex', 'r') as template_file:
+                template = template_file.read()
+                for key in self.report_information["Info Keys"].keys():
+                    template = template.replace(f'${key}$', self.report_information["Info Keys"][key])
+                new_report = template
+
+                filepath = os.path.join(self.path, self.report_information["Info Keys"]["File Name"] + ".tex")
+                with open(filepath, 'w') as new_file:
+                    new_file.write(new_report)
+            
+            print("Report Generated Successfully!")
--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from LaTeXReportGenerator import *
 LaTeX_Report = LaTeXReportGenerator()
 
 LaTeX_Report.get_directory()
+LaTeX_Report.get_template()
 LaTeX_Report.get_report_information()
-LaTeX_Report.generate_report(style="Favero")
+LaTeX_Report.generate_report()
